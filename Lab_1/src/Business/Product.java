25b3e35a55235d53ab414570341eb3e71602f673
Shivi Bhatt <bhatt.s@husky.neu.edu>
2020-01-16 19:58:05 -0500

    Lab 1 Assignment completed

---
+++ Lab_1/src/Business/Product.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author shivibhatt
+ */
+public class Product {
+    //access modifier
+    private String name; 
+    private String price;
+    private String availNum;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
