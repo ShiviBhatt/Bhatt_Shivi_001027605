1a22af37cda312565ba92229b423c4efdf509074
Shivi Bhatt <bhatt.s@husky.neu.edu>
2020-02-15 17:46:53 -0500

    Initial Commit

---
+++ Lab4Skleton/src/Business/Users/Customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Users;
+
+import Business.Abstract.User;
+import java.util.Date;
+
+/**
+ *
+ * @author AEDSpring2019
+ */
+public class Customer {
+    
+}
